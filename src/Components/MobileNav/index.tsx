749cb733e89c4d27fbe893f1b32a11ffe097ae56
Daniel Carvalho <danielcfleite@gmail.com>
2024-02-26 14:35:56 -0300

    fix

--- src/Components/MobileNav/index.tsx
+++ src/Components/MobileNav/index.tsx
@@ -1,9 +1,7 @@
 import Hamburger from 'hamburger-react'
 import { useState } from 'react';
-import { ButtonNav, HamburguerWrapper, Logo, MobileMenu, MobileNavWrapper } from './styles';
+import { ButtonNav, HamburguerWrapper, MobileMenu } from './styles';
 import { colors } from '../../../styles/variables';
-import logo from "../../assets/brand.png";
-import { Link } from 'react-router-dom';
 
 export const MobileNav = () => {
     const [isOpen, setOpen] = useState(false);
