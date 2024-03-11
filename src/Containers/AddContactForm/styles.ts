9afe356a5c0ebe2bf49c0b05caed1fe56b71a376
Daniel Carvalho <danielcfleite@gmail.com>
2024-03-11 16:58:48 -0300

    small-css-fix

--- src/Containers/AddContactForm/styles.ts
+++ src/Containers/AddContactForm/styles.ts
@@ -2,103 +2,109 @@ import styled from "styled-components";
 import { colors } from "../../../styles/variables";
 
 interface SelectionFormProps {
-    select?: boolean;
+  select?: boolean;
 }
 
 interface SocialMediaContainerProps {
-    selected?: boolean
-    theme: {
-        fontColor: string,
-        secondaryFontColor:  string,
-        lowOpacityFontColor:  string,
-        socialIcons:  string,
-        selectedGray: string,
-        lightGray:  string,
-        brand: string,
-        backgroundColor: string,
-    }
+  selected?: boolean;
+  theme: {
+    fontColor: string;
+    secondaryFontColor: string;
+    lowOpacityFontColor: string;
+    socialIcons: string;
+    selectedGray: string;
+    lightGray: string;
+    brand: string;
+    backgroundColor: string;
+  };
 }
 
 export const SectionForm = styled.div<SelectionFormProps>`
-display: flex;
-flex-direction: ${(props) => props.select? "row" : "column" } ;
-align-items: center;
-gap: 1rem;
+  display: flex;
+  flex-direction: ${(props) => (props.select ? "row" : "column")};
+  align-items: center;
+  gap: 1rem;
 
-input{
-    width: ${props => props.select? "60%" : "100%" };
+  input {
+    width: ${(props) => (props.select ? "60%" : "100%")};
     margin: 0;
     padding: 0.7rem 0.5rem;
     border-radius: 10px;
     border: none;
-    background-color: ${props => props.theme.lowOpacityFontColor};
+    background-color: ${(props) => props.theme.lowOpacityFontColor};
 
-    &:focus{
-        outline: 2px solid ${colors.brand};
+    &:focus {
+      outline: 2px solid ${colors.brand};
     }
+  }
 
-}
-
-select {
+  select {
     width: 200px;
     padding: 10px;
     border: 1px solid #ccc;
     border-radius: 5px;
-    background-color: ${props => props.theme.lowOpacityFontColor}; /* Set background color */
+    background-color: ${(props) =>
+      props.theme.lowOpacityFontColor}; /* Set background color */
     font-size: 16px;
-    color: ${props => props.theme.fontColor};
+    color: ${(props) => props.theme.fontColor};
 
     color: black;
-}
+  }
 
-`
+  select option {
+    background-color: ${(props) => props.theme.backgroundColor};
+    font-weight: bold;
+    color: ${(props) => props.theme.fontColor};
+  }
+`;
 
 export const SocialMediaContainer = styled.div<SocialMediaContainerProps>`
-   display: flex;
-    gap: 1rem;
-    align-items: center;
-    margin-bottom: 1rem;
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  margin-bottom: 1rem;
 
-
-    ${props => props.selected ? `
+  ${(props) =>
+    props.selected
+      ? `
         * {
             color: ${colors.brand};
         }
-    ` : `color: ${(props: any) => props.theme.fontcolor};`} // @ts-ignore
+    `
+      : `color: ${(props: any) => props.theme.fontcolor};`} // @ts-ignore
 
     input {
-        width: 70%;
-        padding: 0.7rem 0.5rem;
-        border-radius: 10px;
-        border: none;
-        background-color:   ${props => props.theme.lowOpacityFontColor};
+    width: 70%;
+    padding: 0.7rem 0.5rem;
+    border-radius: 10px;
+    border: none;
+    background-color: ${(props) => props.theme.lowOpacityFontColor};
 
-        &:focus {
-            outline: 2px solid ${colors.brand};
-        }
+    &:focus {
+      outline: 2px solid ${colors.brand};
     }
+  }
 `;
 
-export const Button = styled.button `
+export const Button = styled.button`
+  padding: 0.8rem 1rem;
+  border-radius: 15px;
+  border: none;
+  background-color: ${colors.brand};
+  color: white;
+  cursor: pointer;
+  transition: 0.1s ease-in-out;
+  font-weight: 600;
+  font-size: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 0.7rem;
 
-padding: 0.8rem 1rem ;
-border-radius: 15px;
-border: none;
-background-color: ${colors.brand};
-color: white;
-cursor: pointer;
-transition: 0.1s ease-in-out;
-font-weight: 600;
-font-size: 1rem;
-display: flex;
-align-items: center;
-gap: 0.7rem;
-
-&[type="submit"]{
+  &[type="submit"] {
     margin-top: 3rem;
-}
+  }
 
-&:hover{
-    filter: brightness(1.06)
-}
-`
+  &:hover {
+    filter: brightness(1.06);
+  }
+`;
